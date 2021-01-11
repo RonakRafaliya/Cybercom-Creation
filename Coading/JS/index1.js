345e36cac9ac90997eb375a6425dcf37257865c9
Ronak <Ronakrafaliya14@gmail.com>
2021-01-11 20:01:49 +0530

    Done

---
+++ Coading/JS/index1.js
@@ -0,0 +1,51 @@
+var john_team=[89,120,103];
+var mike_team=[116,94,123];
+
+document.write("John Team:- "+john_team+"<br>")
+document.write("Mike Team:- "+mike_team+"<br><br>")
+
+var jscore=(89+120+103)/3;
+var mscore=(116+94+123)/3;
+
+document.write("John team Avarage Score "+ jscore + "<br>");
+document.write("Mike team Avarage Score "+ mscore + "<br><br>");
+
+if(jscore>mscore)
+{
+	document.write("John win the game with "+ jscore + " Points <br><br>");
+}
+else if(mscore>jscore)
+{
+	document.write("Mike win the game with "+ mscore + " Points <br><br>");
+}
+else
+{
+	document.write("Mtach is Draw");
+}
+
+document.write("*** New Scenario ***<br><br>");
+
+var john_team=[110,120,103];
+var mike_team=[116,94,123];
+
+document.write("John Team:- "+john_team+"<br>")
+document.write("Mike Team:- "+mike_team+"<br><br>")
+
+var jscore=(110+120+103)/3;
+var mscore=(116+94+123)/3;
+
+document.write("John team Avarage Score "+ jscore + "<br>");
+document.write("Mike team Avarage Score "+ mscore + "<br><br>");
+
+if(jscore>mscore)
+{
+	document.write("John win the game with "+ jscore + " Points <br><br>");
+}
+else if(mscore>jscore)
+{
+	document.write("Mike win the game with "+ mscore + " Points <br><br>");
+}
+else
+{
+	document.write("Mtach is Draw");
+}
