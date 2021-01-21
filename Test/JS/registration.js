fd7006b34dc1a702fe641f7739d34fe63fdf1d05
Ronak <Ronakrafaliya14@gmail.com>
2021-01-21 11:42:09 +0530

    commit

--- Test/JS/registration.js
+++ Test/JS/registration.js
@@ -1,3 +1,7 @@
 function validation(){
     var email = document.getElementById('remail').value;
+    var password = document.getElementById('rpassword').value;
+
+    console.log(email);
+    console.log(password);
 }
