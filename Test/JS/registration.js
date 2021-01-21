de72eabc19bbfff822963470ae3ef25e90cf0a02
Ronak <Ronakrafaliya14@gmail.com>
2021-01-21 11:50:48 +0530

    commit

--- Test/JS/registration.js
+++ Test/JS/registration.js
@@ -1,7 +1,16 @@
+let adminEmail = 'dmin@gmail.com';
+let adminPassword = 'admin@123'
+
 function validation(){
     var email = document.getElementById('remail').value;
     var password = document.getElementById('rpassword').value;
 
-    console.log(email);
-    console.log(password);
+    if(email === adminEmail && password === adminPassword)
+    {
+        alert("Admin already Regisrerd");
+        location.replace("login.html")
+    }
+    else{
+        location.replace("dashboard.html");
+    }
 }
