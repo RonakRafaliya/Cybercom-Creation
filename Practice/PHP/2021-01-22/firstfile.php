c53304d0fcd722815f27fbd7c6065a57a2c8df55
Ronak <Ronakrafaliya14@gmail.com>
2021-01-22 18:16:53 +0530

    Done

---
+++ Practice/PHP/2021-01-22/firstfile.php
@@ -0,0 +1,66 @@
+outside content.
+
+<?php
+
+//ini_set('error_reporting', ?);
+
+//echo 'Hello Ronak Rafaliya',"\n";
+//echo 'hello rons';
+//phpinfo();
+
+//Indentation is must require to understand code.
+
+/*$name = 'Rons';
+$age = 22;  //creating variable.
+
+if (strtolower($name) === 'ronak'){
+    if ($age >=22){
+        echo 'you\'are over 22 ';
+        if (1 == 1){
+            echo 'yes, 1 is equal to 1!'; 
+        }
+    }
+    else{
+        echo 'Not over 22'; 
+    }
+}
+else{
+    echo 'You\'re not Ronak';
+}*/
+
+//diaplay html dag using echo.
+
+//echo "<input type = 'text' name = 'name'>";
+
+//embedding php inside html
+
+//$text = 'Hello Ronak'; //refer at the end of html inout tag.
+
+
+
+//concatenation
+/*
+$day='Monday';
+$date=14;
+$year=1998;
+
+echo "the date is $day $date $year","\n"; //double inverted comma. 
+echo 'the date is '.$day.' '.$date.' '.$year; //single  inverted comma.
+*/
+
+$a = 100;
+$b = 50;
+$c = 2;
+
+$a++;
+echo $a;
+++$b;
+echo $b;
+
+
+
+?>
+
+<!--<input type="text" value = "<?php //echo $text; ?>">
+
+
